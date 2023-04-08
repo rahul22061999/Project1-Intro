1e1d79ccf20590568adc3593c9a8034d06426bab
rahul22061999 <130198557+rahul22061999@users.noreply.github.com>
2023-04-08 13:19:45 -0400

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# #### Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 
+# 
+
+# ### Data Science Tools
+# | Data Science Tools |
+# | ------------------ |
+# | Jupyter Notebook   |
+# | Spyder             |
+# | RStudio            |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[6]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# ###### 200 minutes into hours
+
+# In[23]:
+
+
+a = round(200/60,2)
+print(str(a) + '\n' + 'hours')
+
+
+# # Objectives:
+# 1. To learn how to use and manipulate data for business gains 
+# 2. Visualize and data story telling 
+# 3. SKill up and get employed 
+
+# | Popular Languages  |
+# | ------------------ |
+# | Python             |
+# | R                  |
+# | SQL                |
+# | Julia              |
+# 
+
+# ## Author
+# Rahul
+
+# In[ ]:
+
+
+
+
